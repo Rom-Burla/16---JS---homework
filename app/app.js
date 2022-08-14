f0514468354d94680ee4ee4fcd718dfce7cd5440
Rom Burla <romwebdev@gmail.com>
2022-08-14 18:10:24 +0300

    addings and debugs

--- app/app.js
+++ app/app.js
@@ -3,6 +3,12 @@ const arrShapes = ["speed", "dimond", "heart", "club"];
 let arrCards = new Array();
 let counter = 0;
 let myBtn = document.createElement("button");
+document.body.appendChild(myBtn);
+let sortedDeck = document.createElement("div");
+document.body.appendChild(sortedDeck);
+sortedDeck.id = "sorted-deck";
+let compareArr = [];
+
 myBtn.textContent = "Click here to display your deck";
 class Cards {
   constructor(num, suit, id) {
@@ -14,6 +20,13 @@ class Cards {
     card.id = this.id;
     card.textContent = this.num + " " + this.suit;
     myDiv.appendChild(card);
+  }
+  printSorted() {
+    let card = document.createElement("div");
+    card.className = "card";
+    card.id = this.id;
+    card.textContent = this.num + " " + this.suit;
+    sortedDeck.appendChild(card);
   }
 }
 
@@ -67,9 +80,17 @@ while (arrCards.length < 52) {
       for (let i = 0; i < arrCards.length; i++) {
         arrCards[i].printCard();
       }
-
       myDiv.innerHTML += `<div class = "counter">iteritions: ${counter}</div>`;
       myBtn.style.display = "none";
+      let sortBtn = document.createElement("button");
+      myDiv.appendChild(sortBtn);
+      sortBtn.textContent = "Click this button to sort your Deck by numbers";
+      sortBtn.addEventListener("click", () => {
+        arrCards.sort((a, b) => a.num - b.num);
+        for (let i = 0; i < arrCards.length; i++) {
+          arrCards[i].printSorted();
+        }
+      });
     });
     break;
   }
@@ -78,7 +99,10 @@ while (arrCards.length < 52) {
 myDiv.addEventListener("click", (event) => {
   if (event.target.className === "card") {
     event.target.className = "card-back";
-    console.log("card");
+    if (compareArr.length < 2) {
+      compareArr.push(event.target);
+      console.log(compareArr);
+    }
   } else if (event.target.className === "card-back") {
     event.target.className = "card";
   }
